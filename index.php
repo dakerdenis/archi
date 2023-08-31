39bbda68516200ae793d5046a1108ec77023bb24
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-31 10:30:42 +0400

    added

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style/styles.css">
+</head>
+<body>
+    <div class="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde adipisci odio sint sed mollitia qui tenetur dolores, modi officia numquam tempore ad commodi similique! Perferendis quisquam placeat ipsa atque id.
+        <h2 class="header">asdasdas</h2>
+        <h1 class="stalker">asdasdasdasdasd</h1>
+    </div>
+</body>
+</html>
