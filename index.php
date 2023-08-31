6cf7347f28ca6927a215e2a1bcb205e48082dba5
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-08-31 11:00:02 +0400

    added

--- index.php
+++ index.php
@@ -7,10 +7,55 @@
     <link rel="stylesheet" href="./style/styles.css">
 </head>
 <body>
-    <div class="container">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde adipisci odio sint sed mollitia qui tenetur dolores, modi officia numquam tempore ad commodi similique! Perferendis quisquam placeat ipsa atque id.
-        <h2 class="header">asdasdas</h2>
-        <h1 class="stalker">asdasdasdasdasd</h1>
+    <div class="main__wrapper">
+        <header class="header_container">
+            <div class="header_wrapper">
+                <!-------IMAGE-------->
+                <div class="header_image">
+                    <a href="#">
+                        <img src="./imgs/logo.png" alt="Archi logo">
+                    </a>
+                </div>
+                <!-----NAVIGATION------>
+                <div class="header__navigation">
+                    <ul>
+                        <li><a href="#">Книга ремонта</a></li>
+                        <li><a href="#">Построй дом!</a></li>
+                        <li><a href="#">B2B</a></li>
+                        <li><a href="#">О проекте</a></li>
+                    </ul>
+                </div>
+
+                <!------->
+                <div class="header__profile">
+                    <div class="header__profile__image">
+                        <img src="./imgs/profile.png" alt="Profile Logo">
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="main__content">
+            
+        </div>
+
+
+        <footer class="footer">
+            <div class="footer__wrapper">
+                <div class="footer__image__copy">
+                    <div class="footer__image">
+                        <img src="./imgs/logo_white.png" alt="Archi Logo">
+                    </div>
+                    <div class="footer__copy">
+                        <p>©Copyright  2023</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
     </div>
+
+    <script src="./scripts/index.js"></script>
 </body>
 </html>
