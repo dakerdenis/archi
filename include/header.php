2e97cd3a5650277e6cee78eb74535eec329aba34
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-02 15:24:24 +0400

    added

--- include/header.php
+++ include/header.php
@@ -17,12 +17,31 @@
         </div>
 
         <!---PROFILE&LANG---->
-        <div class="header__profile" >
+        <div class="header__profile">
             <div class="header__profile__image" id="dropdown-trigger">
                 <img src="./imgs/profile.png" alt="Profile Logo">
             </div>
             <div class="dropdown" id="dropdown-content">
-                <!-- Dropdown content goes here -->
+                <div class="dropdown_content">
+                    <div class="dropdown__languages">
+                        <a  href="#">
+                            <img src="./imgs/az.png" alt="image" srcset="">
+                        </a>
+                        <a class="lang_active" href="#">
+                            <img src="./imgs/ru.png" alt="image" srcset="">
+                        </a>
+                        <a href="#">
+                            <img src="./imgs/en.png" alt="image" srcset="">
+                        </a>
+                    </div>
+
+                        <a href="./index.php?page=login" class="dropdown__login_block __login__green">
+                            <p>Вход</p>
+                        </a>
+                        <a href="./index.php?page=registration" class="dropdown__login_block">
+                            <p>Регистрация</p>
+                        </a>
+                </div>
             </div>
         </div>
     </div>
