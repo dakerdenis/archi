5af4d27e01cab829de1f902daaf12cd5dce56a89
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-06 10:23:55 +0400

    added

--- include/pages.php
+++ include/pages.php
@@ -50,10 +50,14 @@
             </div>
         </div>
                 <!-------->
-        <div class="pages__page_element">
+                <div class="pages__page_element">
             <div class="pages__page__element__wrapper">
                 <div class="__page__element__image">
-                    <img src="" alt="">
+
+                    <img src="./imgs/1.jpg" alt="">
+                    <div class="__page__element__image__approved">
+                        <p>Archi</p>
+                    </div>
                 </div>
                 <div class="__page__element__content">
                     <div class="__page__element__content_name">
@@ -90,11 +94,15 @@
                 </div>
             </div>
         </div>
-                        <!-------->
-                        <div class="pages__page_element">
+                <!-------->
+                <div class="pages__page_element">
             <div class="pages__page__element__wrapper">
                 <div class="__page__element__image">
-                    <img src="" alt="">
+
+                    <img src="./imgs/1.jpg" alt="">
+                    <div class="__page__element__image__approved">
+                        <p>Archi</p>
+                    </div>
                 </div>
                 <div class="__page__element__content">
                     <div class="__page__element__content_name">
@@ -131,11 +139,15 @@
                 </div>
             </div>
         </div>
-                        <!-------->
-                        <div class="pages__page_element">
+                <!-------->
+                <div class="pages__page_element">
             <div class="pages__page__element__wrapper">
                 <div class="__page__element__image">
-                    <img src="" alt="">
+
+                    <img src="./imgs/1.jpg" alt="">
+                    <div class="__page__element__image__approved">
+                        <p>Archi</p>
+                    </div>
                 </div>
                 <div class="__page__element__content">
                     <div class="__page__element__content_name">
@@ -172,11 +184,15 @@
                 </div>
             </div>
         </div>
-                        <!-------->
-                        <div class="pages__page_element">
+                <!-------->
+                <div class="pages__page_element">
             <div class="pages__page__element__wrapper">
                 <div class="__page__element__image">
-                    <img src="" alt="">
+
+                    <img src="./imgs/1.jpg" alt="">
+                    <div class="__page__element__image__approved">
+                        <p>Archi</p>
+                    </div>
                 </div>
                 <div class="__page__element__content">
                     <div class="__page__element__content_name">
@@ -213,5 +229,51 @@
                 </div>
             </div>
         </div>
+                <!-------->
+                <div class="pages__page_element">
+            <div class="pages__page__element__wrapper">
+                <div class="__page__element__image">
+
+                    <img src="./imgs/1.jpg" alt="">
+                    <div class="__page__element__image__approved">
+                        <p>Archi</p>
+                    </div>
+                </div>
+                <div class="__page__element__content">
+                    <div class="__page__element__content_name">
+                        <p>Изысканный выбор...</p>
+                    </div>
+                    <div class="__page__element__content_tags">
+                        <a href="#">
+                            <p>#Ремонт</p>
+                        </a>
+                        <a href="#">
+                            <p>#Пол</p>
+                        </a>
+                        <a href="#">
+                            <p>#Паркет</p>
+                        </a>
+                    </div>
+                    <div class="__page__element__content_additional">
+                        <div class="__content_additional_element">
+                            <img src="./imgs/Star.svg" alt="">
+                            <p>5.00</p>
+                        </div>
+                        <div class="__content_additional_element">
+                            <img src="./imgs/eye.svg" alt="">
+                            <p>1200</p>
+                        </div>
+                        <div class="__content_additional_element">
+                            <img src="./imgs/clock.svg" alt="">
+                            <p>5 минут</p>
+                        </div>
+                    </div>
+                    <div class="__page__element__content_date">
+                        <p>Добавлено <span>01.04.2023</span> </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
---
+++ include/profile.php
@@ -0,0 +1,39 @@
+<div class="profile_page_wrapper">
+    <div class="profile__content_wrapper">
+        <div class="profile__userdata">
+            <div class="profile__userdata_image">
+                <img src="" alt="">
+            </div>
+            <div class="profile__userdata__content">
+                <div class="profile__userdata__name">
+                    <p>Ахмедджановзаде Ахмед</p>
+                </div>
+                <div class="profile__userdata__element">
+                    <p>Прочитанных статей: 100</p>
+                </div>
+                <div class="profile__userdata__element">
+                    <p>email@gmail.com</p>
+                </div>
+                <div class="profile__userdata__element">
+                    <p>+994 00 000 00 00</p>
+                </div>
+                <div class="profile__userdata__settings">
+                    <a href="#">
+                        <p>Редактировать профиль</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="profile__saved">
+            <div class="profile__saved_savedpages">
+                <p>Сохранённые статьи</p>
+            </div>
+            <div class="profile__saved_savedgoods">
+                <p>Здесь будут ваши сохранённые товары</p>
+            </div>
+        </div>
+    </div>
+    <div class="profile__buisness_section">
+        
+    </div>
+</div>
