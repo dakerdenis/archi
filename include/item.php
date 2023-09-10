9692aa85267e839227124d50eb0f819a04c2ad06
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-10 19:36:53 +0400

    added

--- include/item.php
+++ include/item.php
@@ -1,1 +1,202 @@
-asdasdasd
+<style>
+    <?php include './style/article.css' ?><?php include './style/item.css' ?>
+</style>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
+
+
+<div class="item__page__wrapper">
+    <div class="item__page__container">
+        <!----ITEM----->
+        <div class="item__block">
+            <!----Item FOTO AND DESCRIPTION SMALL--->
+            <div class="item__content__block">
+                <!---FOTO-->
+                <div class="item__image">
+                    <img src="./imgs/.jpg" alt="" srcset="">
+                </div>
+                <!-----description of item-->
+                <div class="item__content__desc">
+                    <div class="item__name">
+                        <p>Название товара 229 üəöğşç</p>
+                    </div>
+                    <div class="item__price">
+                        <p>25.00 AZN</p>
+                    </div>
+                    <div class="item__tags">
+                        <a href="#">
+                            <p>#Ремонт</p>
+                        </a>
+                        <a href="#">
+                            <p>#Пол</p>
+                        </a>
+                        <a href="#">
+                            <p>#Паркет</p>
+                        </a>
+                    </div>
+                    <div class="item__date">
+                        <p>Добавлено: 01.04.2023</p>
+                    </div>
+                    <div class="item__number">
+                        <a href="#">
+                            +994 50 750 69 01
+                        </a>
+                    </div>
+                    <div class="item__number__desc">
+                        <p>Нажмите что бы увидеть номер</p>
+                    </div>
+                </div>
+            </div>
+            <!-------Item FULL DESCRIPTION---->
+            <div class="item__description">
+                <div class="item__description__country">
+                    <p>Страна:</p>
+                </div>
+                <div class="item__description__color">
+                    <p>Цвет:</p>
+                </div>
+                <div class="item__description__text">
+                    <p>Преимущества паркетных полов:
+                        Изысканный внешний вид: паркетные полы придают интерьеру шик и элегантность, добавляя в помещение теплоту и уют.
+                    </p>
+                    <p>
+                        Прочность и долговечность: паркетные полы изготовлены из натурального дерева, что делает их прочными и долговечными.
+                        Правильно уложенный и ухоженный паркетный пол может прослужить десятилетиями.
+                    </p>
+                    <p>
+                        Уникальный дизайн: паркетные полы предлагают множество вариантов дизайна, таких как разные оттенки древесины,
+                        различные узоры и мозаики, позволяя создать уникальный внешний вид пола, соответствующий вашему стилю и предпочтениям.
+
+                    </p>
+                    <p>
+                        Экологическая чистота: паркетные полы изготовлены из натурального дерева, что делает их экологически чистым
+                        и безопасным выбором для вашего дома/
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!---ITEM RECOMENDATIONS---->
+        <div class="item__recomendation__container">
+
+        </div>
+    </div>
+    <div class="item__articles">
+        <div class="article__recomendation">
+            <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="swiper-slide">
+                        <a href="#" class="swiper__article__recomendation__block">
+                            <div class="article__recomendation__image">
+                                <img src="./imgs/1.jpg" alt="">
+                            </div>
+                            <div class="article__recomendation__name">
+                                <p>Название статьи</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<!-- Swiper JS -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
+<!-- Initialize Swiper -->
+<script>
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        freeMode: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
+</script>
--- include/login.php
+++ include/login.php
@@ -27,8 +27,30 @@
     </div>
 
     <div class="login__page__advertisement">
-    <div class="advertisement__block">
-                <div class="advertisement__blockBig">
+        <div class="advertisement__block">
+            <div class="advertisement__blockBig">
+                <!---CONTENT BLOCK----->
+                <div class="advertisement__block__content">
+                    <img src="./imgs/1.jpg" alt="Advertisement">
+                    <div class="advertisement__block__maincontent">
+                        <div class="advertisement__block__text">
+                            <div class="advertisement__block__name">
+                                <p>Доставка керамогранита</p>
+                            </div>
+                            <div class="advertisement__block__desc">
+                                <p>
+                                    Компания «G-Stone» является лидером
+                                    в поставках самого качественного и
+                                    долговечного керамогранита.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="advertisement__blockBig">
+
+                <div class="advertisement__blockSmall">
                     <!---CONTENT BLOCK----->
                     <div class="advertisement__block__content">
                         <img src="./imgs/1.jpg" alt="Advertisement">
@@ -48,89 +70,67 @@
                         </div>
                     </div>
                 </div>
-                <div class="advertisement__blockBig">
-
-                    <div class="advertisement__blockSmall">
-                        <!---CONTENT BLOCK----->
-                        <div class="advertisement__block__content">
-                            <img src="./imgs/1.jpg" alt="Advertisement">
-                            <div class="advertisement__block__maincontent">
-                                <div class="advertisement__block__text">
-                                    <div class="advertisement__block__name">
-                                        <p>Доставка керамогранита</p>
-                                    </div>
-                                    <div class="advertisement__block__desc">
-                                        <p>
-                                            Компания «G-Stone» является лидером
-                                            в поставках самого качественного и
-                                            долговечного керамогранита.
-                                        </p>
-                                    </div>
+                <div class="advertisement__blockSmall">
+                    <!---CONTENT BLOCK----->
+                    <div class="advertisement__block__content">
+                        <img src="./imgs/1.jpg" alt="Advertisement">
+                        <div class="advertisement__block__maincontent">
+                            <div class="advertisement__block__text">
+                                <div class="advertisement__block__name">
+                                    <p>Доставка керамогранита</p>
+                                </div>
+                                <div class="advertisement__block__desc">
+                                    <p>
+                                        Компания «G-Stone» является лидером
+                                        в поставках самого качественного и
+                                        долговечного керамогранита.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="advertisement__blockSmall">
-                        <!---CONTENT BLOCK----->
-                        <div class="advertisement__block__content">
-                            <img src="./imgs/1.jpg" alt="Advertisement">
-                            <div class="advertisement__block__maincontent">
-                                <div class="advertisement__block__text">
-                                    <div class="advertisement__block__name">
-                                        <p>Доставка керамогранита</p>
-                                    </div>
-                                    <div class="advertisement__block__desc">
-                                        <p>
-                                            Компания «G-Stone» является лидером
-                                            в поставках самого качественного и
-                                            долговечного керамогранита.
-                                        </p>
-                                    </div>
+                </div>
+                <div class="advertisement__blockSmall">
+                    <!---CONTENT BLOCK----->
+                    <div class="advertisement__block__content">
+                        <img src="./imgs/1.jpg" alt="Advertisement">
+                        <div class="advertisement__block__maincontent">
+                            <div class="advertisement__block__text">
+                                <div class="advertisement__block__name">
+                                    <p>Доставка керамогранита</p>
+                                </div>
+                                <div class="advertisement__block__desc">
+                                    <p>
+                                        Компания «G-Stone» является лидером
+                                        в поставках самого качественного и
+                                        долговечного керамогранита.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="advertisement__blockSmall">
-                        <!---CONTENT BLOCK----->
-                        <div class="advertisement__block__content">
-                            <img src="./imgs/1.jpg" alt="Advertisement">
-                            <div class="advertisement__block__maincontent">
-                                <div class="advertisement__block__text">
-                                    <div class="advertisement__block__name">
-                                        <p>Доставка керамогранита</p>
-                                    </div>
-                                    <div class="advertisement__block__desc">
-                                        <p>
-                                            Компания «G-Stone» является лидером
-                                            в поставках самого качественного и
-                                            долговечного керамогранита.
-                                        </p>
-                                    </div>
+                </div>
+                <div class="advertisement__blockSmall">
+                    <!---CONTENT BLOCK----->
+                    <div class="advertisement__block__content">
+                        <img src="./imgs/1.jpg" alt="Advertisement">
+                        <div class="advertisement__block__maincontent">
+                            <div class="advertisement__block__text">
+                                <div class="advertisement__block__name">
+                                    <p>Доставка керамогранита</p>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="advertisement__blockSmall">
-                        <!---CONTENT BLOCK----->
-                        <div class="advertisement__block__content">
-                            <img src="./imgs/1.jpg" alt="Advertisement">
-                            <div class="advertisement__block__maincontent">
-                                <div class="advertisement__block__text">
-                                    <div class="advertisement__block__name">
-                                        <p>Доставка керамогранита</p>
-                                    </div>
-                                    <div class="advertisement__block__desc">
-                                        <p>
-                                            Компания «G-Stone» является лидером
-                                            в поставках самого качественного и
-                                            долговечного керамогранита.
-                                        </p>
-                                    </div>
+                                <div class="advertisement__block__desc">
+                                    <p>
+                                        Компания «G-Stone» является лидером
+                                        в поставках самого качественного и
+                                        долговечного керамогранита.
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     </div>
 </div>
