a394984bdfa5683083cc8eb5a0e4c475a3d0b441
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-11 13:53:21 +0400

    added

--- include/b2b/mainpage.php
+++ include/b2b/mainpage.php
@@ -1,3 +1,7 @@
+<style>
+    <?php include './style/b2b/mainpage.css' ?>
+</style>
+
 <div class="b2b__main__page">
     <div class="b2b__main__wrapper">
         <!----->
@@ -32,9 +36,9 @@
                 <p>Поиск</p>
             </div>
             <div class="main__search__wrapper">
-                <div class="main__search__element">
+            <div class="main__search__element">
                     <div class="search__element__logo">
-                        <img src="./imgs/main_logo.png" alt="">
+                        <img src="./imgs/6.png" alt="">
                     </div>
                     <div class="search__element__desc">
                         <div class="search__element__desc__name">
@@ -47,6 +51,216 @@
                         <div class="search__element__desc__tags">
                             <a href="#">
                                 <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="search__element__info">
+                        <a href="#">
+                            <p>Подробнее</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="main__search__element">
+                    <div class="search__element__logo">
+                        <img src="./imgs/6.png" alt="">
+                    </div>
+                    <div class="search__element__desc">
+                        <div class="search__element__desc__name">
+                            <p>Nazvaniye Kompanii asdzx zxıəşəqüö çş lı</p>
+                        </div>
+                        <div class="search__element__desc__location">
+                            <p>Страна: Россия</p>
+                            <p>Город: Нижневартовск</p>
+                        </div>
+                        <div class="search__element__desc__tags">
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="search__element__info">
+                        <a href="#">
+                            <p>Подробнее</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="main__search__element">
+                    <div class="search__element__logo">
+                        <img src="./imgs/6.png" alt="">
+                    </div>
+                    <div class="search__element__desc">
+                        <div class="search__element__desc__name">
+                            <p>Nazvaniye Kompanii asdzx zxıəşəqüö çş lı</p>
+                        </div>
+                        <div class="search__element__desc__location">
+                            <p>Страна: Россия</p>
+                            <p>Город: Нижневартовск</p>
+                        </div>
+                        <div class="search__element__desc__tags">
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="search__element__info">
+                        <a href="#">
+                            <p>Подробнее</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="main__search__element">
+                    <div class="search__element__logo">
+                        <img src="./imgs/6.png" alt="">
+                    </div>
+                    <div class="search__element__desc">
+                        <div class="search__element__desc__name">
+                            <p>Nazvaniye Kompanii asdzx zxıəşəqüö çş lı</p>
+                        </div>
+                        <div class="search__element__desc__location">
+                            <p>Страна: Россия</p>
+                            <p>Город: Нижневартовск</p>
+                        </div>
+                        <div class="search__element__desc__tags">
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="search__element__info">
+                        <a href="#">
+                            <p>Подробнее</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="main__search__element">
+                    <div class="search__element__logo">
+                        <img src="./imgs/6.png" alt="">
+                    </div>
+                    <div class="search__element__desc">
+                        <div class="search__element__desc__name">
+                            <p>Nazvaniye Kompanii asdzx zxıəşəqüö çş lı</p>
+                        </div>
+                        <div class="search__element__desc__location">
+                            <p>Страна: Россия</p>
+                            <p>Город: Нижневартовск</p>
+                        </div>
+                        <div class="search__element__desc__tags">
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="search__element__info">
+                        <a href="#">
+                            <p>Подробнее</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="main__search__element">
+                    <div class="search__element__logo">
+                        <img src="./imgs/6.png" alt="">
+                    </div>
+                    <div class="search__element__desc">
+                        <div class="search__element__desc__name">
+                            <p>Nazvaniye Kompanii asdzx zxıəşəqüö çş lı</p>
+                        </div>
+                        <div class="search__element__desc__location">
+                            <p>Страна: Россия</p>
+                            <p>Город: Нижневартовск</p>
+                        </div>
+                        <div class="search__element__desc__tags">
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#пол</p>
+                            </a>
+                            <a href="#">
+                                <p>#ведро</p>
+                            </a>
+                            <a href="#">
+                                <p>#Ремонт</p>
+                            </a>
+                            <a href="#">
+                                <p>#Машина</p>
+                            </a>
+                            <a href="#">
+                                <p>#Сталкер</p>
                             </a>
                         </div>
                     </div>
