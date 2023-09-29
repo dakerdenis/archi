c42b381ebb1600290740c4cad7f73a3904fda522
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-29 09:45:12 +0400

    added

--- include/b2b/registration.php
+++ include/b2b/registration.php
@@ -10,38 +10,26 @@
     <div class="login__page__form">
         <form action="#" method="post">
             <div class="login__input__container">
-                <input type="text" placeholder="ФИО">
+                <input type="text" placeholder="Название компании">
             </div>
             <div class="login__input__container">
-                <input type="text" placeholder="Номер телефона">
+                <input type="text" placeholder="ФИО представителя">
             </div>
             <div class="login__input__container">
-                <input type="text" placeholder="Адрес почты">
+                <input type="number" placeholder="Номер телефона">
             </div>
             <div class="login__input__container">
-                <input type="text" placeholder="Пароль">
+                <input type="text" placeholder="E-mail адрес">
             </div>
             <div class="login__input__container">
-                <input type="text" placeholder="Подтвердите пароль">
-            </div>
-            <div class="login__input__gender">
-                <div class="login__input__gender_wrapper">
-                    <label>
-                        <input type="radio" name="custom-radio" value="option1">
-                        <span class="custom-radio"></span> Мужчина
-                    </label>
-                    <label>
-                        <input type="radio" name="custom-radio" checked value="option2">
-                        <span class="custom-radio"></span> Женщина
-                    </label>
-                </div>  
+                <input type="text" placeholder="*дополнительный E-mail адрес">
             </div>
             <div class="login__input__checkbox">  
-                    <label class="custom-checkbox">
-                         <input type="checkbox" class="hidden-checkbox">
-                         <span class="checkmark"></span>
-                    </label>
-                    <p>Я согласен с <a href="#">пользовательским соглашением</a></p>
+                <label class="custom-checkbox">
+                     <input type="checkbox" class="hidden-checkbox">
+                     <span class="checkmark"></span>
+                </label>
+                <p>Я согласен с <a href="#">пользовательским соглашением</a></p>
             </div>
             <div class="login__input__button login__input__button_registration">
                 <button>Регистрация</button>
