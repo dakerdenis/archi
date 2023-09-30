4b61c2f257f57678517b5ba3ce506f20d1cf0d83
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-30 12:35:22 +0300

    added

--- include/new_buisness.php
+++ include/new_buisness.php
@@ -6,5 +6,69 @@
         <div class="new_buisness__name">
             <p>Добавление бизнеса</p>
         </div>
+        <form action="" class="new_buisness__form">
+            <div class="new_buisness__form__wrapper">
+                <!----название компании---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__wrapper">
+                        <input type="text" placeholder="Название компании">
+                    </div>
+                </div>
+                <!----Страна город---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__wrapper new_buisness__double-input">
+                        <input type="text">
+                        <input type="text">
+                    </div>
+                </div>
+                <!----Дата основания и выбор категорий---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__wrapper new_buisness__double-input">
+                        <input type="text">
+                        <input type="text">
+                    </div>
+                </div>
+                <!----Контакты компани + email---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__name">
+                        <p>Контакты компании</p>
+                    </div>
+                    <div class="new_buisness__input__wrapper new_buisness__double-input">
+                        <input type="text">
+                        <input type="text" placeholder="email адрес">
+                    </div>
+                </div>
+                <!----Контакты преставителя компании---->
+                <!---Контакты представитея - ФИО  ---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__name">
+                        <p>Контакты представителя компании*</p>
+                    </div>
+                    <div class="new_buisness__input__wrapper">
+                        <input type="text" placeholder="ФИО представителя">
+                    </div>
+                </div>
+                <!---Контакты представитея - номер + email  ---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__wrapper new_buisness__double-input">
+                        <input type="text">
+                        <input type="text" placeholder="email адрес">
+                    </div>
+                </div>
+
+                <!----Добавление медиафайлов---->
+                <div class="new_buisness__input__container">
+                    <div class="new_buisness__input__name">
+                        <p>Добавление медиафайлов</p>
+                    </div>
+                </div>
+                <!----Добавление сертификатов---->
+                <div class="new_buisness__input__container">
+
+                </div>
+
+
+            </div>
+        </form>
     </div>
 </div>
