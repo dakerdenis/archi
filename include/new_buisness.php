b4a06e0a54fb9c78b533dd8c4b7ea8d3111fc16b
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-09-30 13:44:20 +0300

    added

--- include/new_buisness.php
+++ include/new_buisness.php
@@ -17,15 +17,53 @@
                 <!----Страна город---->
                 <div class="new_buisness__input__container">
                     <div class="new_buisness__input__wrapper new_buisness__double-input">
-                        <input type="text">
-                        <input type="text">
+                        <div class="custom-dropdown" id="dropdown1">
+                            <input type="text" class=" dropdown-input" placeholder="Страна" readonly>
+                            <div class="arrow"></div>
+                            <ul class="dropdown-list">
+                                <li data-value="Скеллиге">Скеллиге</li>
+                                <li data-value="Редания">Редания</li>
+                                <li data-value="Темерия">Темерия</li>
+                                <!-- Add more cities as needed -->
+                            </ul>
+                        </div>
+
+                        <div class="custom-dropdown" id="dropdown2">
+                            <input type="text" class=" dropdown-input" placeholder="Город" readonly>
+                            <div class="arrow"></div>
+                            <ul class="dropdown-list">
+                                <li data-value="Oksenfurt">Oksenfurt</li>
+                                <li data-value="Redaniya North">Redaniya North</li>
+                                <li data-value="Ümlereich Süd">Ümlereich Süd</li>
+                                <!-- Add more cities as needed -->
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <!----Дата основания и выбор категорий---->
                 <div class="new_buisness__input__container">
                     <div class="new_buisness__input__wrapper new_buisness__double-input">
-                        <input type="text">
-                        <input type="text">
+                        <div class="custom-datepicker">
+                            <input type="text" class="datepicker-input" placeholder="Дата основания" readonly>
+                            <div class="datepicker-popup">
+                                <div class="datepicker-header">
+                                    <select class="year-select"></select>
+                                    <select class="month-select"></select>
+                                </div>
+                                <div class="datepicker-days"></div>
+                            </div>
+                        </div>
+
+                        <div class="custom-dropdown" id="dropdown3">
+                            <input type="text" class=" dropdown-input" placeholder="Выбор категорий" readonly>
+                            <div class="arrow"></div>
+                            <ul class="dropdown-list">
+                                <li data-value="Мечи">Мечи</li>
+                                <li data-value="Нагрудные латы">Нагрудные латы</li>
+                                <li data-value="Das Üngefer Einige">Das Üngefer Einige</li>
+                                <!-- Add more cities as needed -->
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <!----Контакты компани + email---->
@@ -34,8 +72,8 @@
                         <p>Контакты компании</p>
                     </div>
                     <div class="new_buisness__input__wrapper new_buisness__double-input">
-                        <input type="text">
-                        <input type="text" placeholder="email адрес">
+                        <input type="number" placeholder="+994 (**) *** ** **">
+                        <input type="email" placeholder="e-mail адрес">
                     </div>
                 </div>
                 <!----Контакты преставителя компании---->
@@ -51,8 +89,8 @@
                 <!---Контакты представитея - номер + email  ---->
                 <div class="new_buisness__input__container">
                     <div class="new_buisness__input__wrapper new_buisness__double-input">
-                        <input type="text">
-                        <input type="text" placeholder="email адрес">
+                        <input type="number" placeholder="+994 (**) *** ** **">
+                        <input type="email" placeholder="e-mail адрес">
                     </div>
                 </div>
 
@@ -72,3 +110,133 @@
         </form>
     </div>
 </div>
+
+
+<script>
+    $(document).ready(function() {
+        // Add a click event handler for all custom-dropdowns
+        $(".custom-dropdown").each(function() {
+            const $dropdown = $(this);
+            const $input = $dropdown.find(".dropdown-input");
+            const $list = $dropdown.find(".dropdown-list");
+
+            $input.click(function() {
+                $dropdown.toggleClass("open"); // Add or remove the 'open' class
+                $list.toggle();
+            });
+
+            $list.find("li").click(function() {
+                const selectedCity = $(this).attr("data-value");
+                $input.val(selectedCity);
+                $input.prop("readonly", true);
+                $list.hide();
+                $dropdown.removeClass("open"); // Remove 'open' class when the list is closed
+            });
+
+            // Close the dropdown when clicking outside
+            $(document).click(function(event) {
+                if (!$(event.target).closest(".custom-dropdown").is($dropdown)) {
+                    $input.prop("readonly", true);
+                    $list.hide();
+                    $dropdown.removeClass("open"); // Remove 'open' class when the list is closed
+                }
+            });
+        });
+    });
+
+
+
+
+
+
+    $(document).ready(function() {
+        const $datepicker = $(".custom-datepicker");
+        const $input = $datepicker.find(".datepicker-input");
+        const $popup = $datepicker.find(".datepicker-popup");
+
+        // Function to populate the year and month selects
+        function populateYearMonthSelects() {
+            const currentYear = new Date().getFullYear();
+            const $yearSelect = $popup.find(".year-select");
+            const $monthSelect = $popup.find(".month-select");
+
+            for (let year = currentYear - 30; year <= currentYear + 5; year++) {
+                $yearSelect.append(`<option value="${year}">${year}</option>`);
+            }
+
+            const months = [
+                "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"
+            ];
+
+            for (let i = 0; i < months.length; i++) {
+                $monthSelect.append(`<option value="${i}">${months[i]}</option>`);
+            }
+        }
+
+        // Function to generate and populate the days grid
+        function populateDaysGrid(year, month) {
+            const $daysGrid = $popup.find(".datepicker-days");
+            $daysGrid.empty();
+
+            // Calculate the first day of the month (0 = Sunday, 1 = Monday, etc.)
+            const firstDayOfMonth = new Date(year, month, 1).getDay();
+
+            // Calculate the number of days in the selected month
+            const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+            // Fill in empty cells before the first day of the month
+            for (let i = 0; i < firstDayOfMonth; i++) {
+                $daysGrid.append("<div></div>");
+            }
+
+            // Populate the days
+            for (let day = 1; day <= daysInMonth; day++) {
+                $daysGrid.append(`<div>${day}</div>`);
+            }
+        }
+
+        // Toggle date picker visibility
+        $input.click(function() {
+            $popup.toggleClass("open");
+            if ($popup.hasClass("open")) {
+                populateYearMonthSelects();
+                const selectedYear = parseInt($popup.find(".year-select").val());
+                const selectedMonth = parseInt($popup.find(".month-select").val());
+                populateDaysGrid(selectedYear, selectedMonth);
+            }
+        });
+
+        // Handle year and month selection
+        $popup.find(".year-select, .month-select").change(function() {
+            const selectedYear = parseInt($popup.find(".year-select").val());
+            const selectedMonth = parseInt($popup.find(".month-select").val());
+            populateDaysGrid(selectedYear, selectedMonth);
+        });
+
+        // Handle day selection
+        $popup.on("click", ".datepicker-days div", function() {
+            const selectedYear = parseInt($popup.find(".year-select").val());
+            const selectedMonth = parseInt($popup.find(".month-select").val()) + 1; // Add 1 to month to match human-readable format
+            const selectedDay = parseInt($(this).text());
+
+            // Format the date as "day.month.year"
+            const formattedDate = `${selectedDay}.${selectedMonth}.${selectedYear}`;
+
+            // Update the input field with the formatted date
+            $input.val(formattedDate);
+
+            // Close the date picker
+            $popup.removeClass("open");
+        });
+
+
+        // Close date picker when clicking outside
+        $(document).click(function(event) {
+            if (!$(event.target).closest(".custom-datepicker").length) {
+                $popup.removeClass("open");
+            }
+        });
+    });
+</script>
