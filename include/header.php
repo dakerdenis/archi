aae9cff1519eadb65ca7b6dbcf2dd2c17611c1f2
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-03 12:03:22 +0400

    added

--- include/header.php
+++ include/header.php
@@ -60,7 +60,41 @@
                 </div>
 
                 <div class="burger__menu__content" id="burger-menu-content">
-                        <a href="./index.php?page=pages">asdasd</a>
+                        <div class="burger__menu__languages">
+                            <a class="" href="#">
+                                <p>AZ</p>
+                            </a>
+                            <a class="burger__menu__languages__active" href="#">
+                                <p>RU</p>
+                            </a>
+                            <a class="" href="#">
+                                <p>ENG</p>
+                            </a>
+                        </div>
+                        <div class="burger__menu__login">
+                            <a href="./index.php?page=login">
+                                <p>Вход</p>
+                            </a>
+                        </div>
+                        <div class="burger__menu__registration">
+                            <a href="./index.php?page=registration">
+                                <p>Регистрация</p>
+                            </a>
+                        </div>
+                        <div class="burger__menu__links">
+                            <a href="./index.php?page=pages">
+                                <p>Статьи</p>
+                            </a>
+                            <a href="./index.php?page=goods">
+                                <p>Построй дом!</p>
+                            </a>
+                            <a href="./b2b.php">
+                                <p>B2B</p>
+                            </a>
+                            <a href="#">
+                                <p>О проекте</p>
+                            </a>
+                        </div>
                 </div>
             </div>
         </div>
