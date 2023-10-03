eb0ade5767268840e010ef53a0c426f5dbeeda7f
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-03 13:34:48 +0400

    finished "my pages" page

--- include/my_article_list.php
+++ include/my_article_list.php
@@ -1,1 +1,123 @@
-dasdasd
+<style>
+    <?php include './style/my_article.css' ?>
+</style>
+
+<div class="my__article__container">
+    <div class="my__article__name">
+        <p>Мои статьи</p>
+    </div>
+    <div class="my__article__search__add">
+        <div class="my__article__search">
+           <img src="./imgs/search.png" alt=""> 
+           <input type="text" placeholder="Введите название статьи...">
+        </div>
+        <div class="my__article__addnew">
+            <a href="#">
+                <p>+ Добавить Статью</p>
+            </a>
+        </div>
+    </div>
+
+    <div class="my__article__list">
+        <div class="my__article__list__wrapper">
+            <div class="my__article__element">
+                <div class="my__article__element__name">
+                    <p>Название статьи по продаже камней</p>
+                </div>
+                <div class="my__article__edit-delete">
+                    <div class="my__article__element__edit">
+                        <a href="#">
+                            <img src="./imgs/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="my__article__element__delete">
+                        <a href="#">
+                            <img src="./imgs/delete.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my__article__element">
+                <div class="my__article__element__name">
+                    <p>Название статьи по продаже камней</p>
+                </div>
+                <div class="my__article__edit-delete">
+                    <div class="my__article__element__edit">
+                        <a href="#">
+                            <img src="./imgs/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="my__article__element__delete">
+                        <a href="#">
+                            <img src="./imgs/delete.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="my__article__element">
+                <div class="my__article__element__name">
+                    <p>Название статьи по продаже камней</p>
+                </div>
+                <div class="my__article__edit-delete">
+                    <div class="my__article__element__edit">
+                        <a href="#">
+                            <img src="./imgs/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="my__article__element__delete">
+                        <a href="#">
+                            <img src="./imgs/delete.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my__article__element">
+                <div class="my__article__element__name">
+                    <p>Название статьи по продаже камней</p>
+                </div>
+                <div class="my__article__edit-delete">
+                    <div class="my__article__element__edit">
+                        <a href="#">
+                            <img src="./imgs/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="my__article__element__delete">
+                        <a href="#">
+                            <img src="./imgs/delete.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="my__article__element">
+                <div class="my__article__element__name">
+                    <p>Название статьи по продаже камней</p>
+                </div>
+                <div class="my__article__edit-delete">
+                    <div class="my__article__element__edit">
+                        <a href="#">
+                            <img src="./imgs/edit.png" alt="">
+                        </a>
+                    </div>
+                    <div class="my__article__element__delete">
+                        <a href="#">
+                            <img src="./imgs/delete.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="my__article__list__show-more">
+            <button>
+                <p>Показать ещё</p>
+            </button>
+        </div>
+    </div>
+</div>
