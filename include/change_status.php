e0a2457995048a920bdd00376876b7f95b02ff4d
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-08 12:44:07 +0300

    added

--- include/change_status.php
+++ include/change_status.php
@@ -14,25 +14,132 @@
                     <div class="change__status__element__content">
                         <div class="change__status__element__desc">
                             <p>
-                            Количество категорий: 1
+                                Количество категорий: 1
                             </p>
                             <p>
-                            Количество подкатегорий: 3
+                                Количество подкатегорий: 3
                             </p>
                             <p>
-                            Количество товаров: 5
+                                Количество товаров: 5
                             </p>
                         </div>
                         <div class="change__status__element__price">
-
+                            <p>БЕСПЛАТНО</p>
                         </div>
                     </div>
                 </div>
 
                 <div class="change__status__element__change">
-
+                    <button>
+                        Сменить статус
+                    </button>
                 </div>
             </div>
+            <!------>
+            <div class="change__status__element">
+                <div class="change__status__element__wrapper">
+                    <div class="change__status__element__image">
+                        <img src="./imgs/change_status.svg" alt="">
+                    </div>
+
+                    <div class="change__status__element__content">
+                        <div class="change__status__element__desc">
+                            <p>
+                                Количество категорий: 2
+                            </p>
+                            <p>
+                                Количество подкатегорий: 5
+                            </p>
+                            <p>
+                                Количество товаров: 7
+                            </p>
+                        </div>
+                        <div class="change__status__element__price">
+                            <p>20 AZN в месяц</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="change__status__element__change">
+                    <button>
+                        Сменить статус
+                    </button>
+                </div>
+            </div>
+            <!------>
+            <div class="change__status__element">
+                <div class="change__status__element__wrapper">
+                    <div class="change__status__element__image">
+                        <img src="./imgs/change_status.svg" alt="">
+                    </div>
+
+                    <div class="change__status__element__content">
+                        <div class="change__status__element__desc">
+                            <p>
+                                Количество категорий: 3
+                            </p>
+                            <p>
+                                Количество подкатегорий: 8
+                            </p>
+                            <p>
+                                Количество товаров: 10
+                            </p>
+                            <p>
+                                Статьи: 1
+                            </p>
+                        </div>
+                        <div class="change__status__element__price">
+                            <p>50 AZN в месяц</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="change__status__element__change">
+                    <button>
+                        Сменить статус
+                    </button>
+                </div>
+            </div>
+            <!------>
+            <div class="change__status__element">
+                <div class="change__status__element__wrapper">
+                    <div class="change__status__element__image">
+                        <img src="./imgs/change_status.svg" alt="">
+                    </div>
+
+                    <div class="change__status__element__content">
+                        <div class="change__status__element__desc">
+                            <p>
+                                Количество категорий: 5
+                            </p>
+                            <p>
+                                Количество подкатегорий: 10
+                            </p>
+                            <p>
+                                Количество товаров: 100
+                            </p>
+                            <p>Статьи: 3</p>
+                            <p>Попадание статей
+                                в рекламный блок</p>
+                        </div>
+                        <div class="change__status__element__price">
+                            <p>150 AZN в месяц</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="change__status__element__change">
+                    <button>
+                        Сменить статус
+                    </button>
+                </div>
+            </div>
+
+
+
+
+
+
         </div>
     </div>
 </div>
