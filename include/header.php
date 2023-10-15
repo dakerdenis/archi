60f0b7d658635fa3135c229331de0077438179ad
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-15 22:44:32 +0400

    фввув

--- include/header.php
+++ include/header.php
@@ -9,8 +9,20 @@
         <!-----NAVIGATION------>
         <div class="header__navigation">
             <ul>
-                <li><a href="./index.php?page=pages">Статьи</a></li>
-                <li><a href="./index.php?page=goods">Построй дом!</a></li>
+                <li><a href="./index.php?page=pages" style="
+                <?php if (isset($_GET['page'])) {
+                    if ($_GET['page'] == "pages") {
+                        echo "background-color: #FFFF00;";
+                    }
+                } ?>
+                ">Статьи</a></li>
+                <li><a href="./index.php?page=goods" style="
+                                    <?php if (isset($_GET['page'])) {
+                    if ($_GET['page'] == "goods") {
+                        echo "background-color: #FFFF00;";
+                    }
+                } ?>
+                ">Построй дом!</a></li>
                 <li><a href="./b2b.php">B2B</a></li>
                 <li><a href="#">О проекте</a></li>
             </ul>
@@ -42,56 +54,56 @@
 
 
         </div>
-    <!-----burger button and menu----->
-    <div class="header__burger_menu">
-        <div class="menu-container">
-            <div class="burger-menu" id="burger-menu">
-                <div class="burger-icon">
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                    <div class="bar"></div>
+        <!-----burger button and menu----->
+        <div class="header__burger_menu">
+            <div class="menu-container">
+                <div class="burger-menu" id="burger-menu">
+                    <div class="burger-icon">
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                        <div class="bar"></div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="burger__menu__content" id="burger-menu-content">
-                <div class="burger__menu__languages">
-                    <a class="" href="#">
-                        <p>AZ</p>
-                    </a>
-                    <a class="burger__menu__languages__active" href="#">
-                        <p>RU</p>
-                    </a>
-                    <a class="" href="#">
-                        <p>ENG</p>
-                    </a>
-                </div>
-                <div class="burger__menu__login">
-                    <a href="./index.php?page=login">
-                        <p>Вход</p>
-                    </a>
-                </div>
-                <div class="burger__menu__registration">
-                    <a href="./index.php?page=registration">
-                        <p>Регистрация</p>
-                    </a>
-                </div>
-                <div class="burger__menu__links">
-                    <a href="./index.php?page=pages">
-                        <p>Статьи</p>
-                    </a>
-                    <a href="./index.php?page=goods">
-                        <p>Построй дом!</p>
-                    </a>
-                    <a href="./b2b.php">
-                        <p>B2B</p>
-                    </a>
-                    <a href="#">
-                        <p>О проекте</p>
-                    </a>
+                <div class="burger__menu__content" id="burger-menu-content">
+                    <div class="burger__menu__languages">
+                        <a class="" href="#">
+                            <p>AZ</p>
+                        </a>
+                        <a class="burger__menu__languages__active" href="#">
+                            <p>RU</p>
+                        </a>
+                        <a class="" href="#">
+                            <p>ENG</p>
+                        </a>
+                    </div>
+                    <div class="burger__menu__login">
+                        <a href="./index.php?page=login">
+                            <p>Вход</p>
+                        </a>
+                    </div>
+                    <div class="burger__menu__registration">
+                        <a href="./index.php?page=registration">
+                            <p>Регистрация</p>
+                        </a>
+                    </div>
+                    <div class="burger__menu__links">
+                        <a href="./index.php?page=pages">
+                            <p>Статьи</p>
+                        </a>
+                        <a href="./index.php?page=goods">
+                            <p>Построй дом!</p>
+                        </a>
+                        <a href="./b2b.php">
+                            <p>B2B</p>
+                        </a>
+                        <a href="#">
+                            <p>О проекте</p>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
 
     </div>
