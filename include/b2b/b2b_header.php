394c9edffecb613009bfb3c12527947921e299da
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-18 23:40:09 +0400

    added

--- include/b2b/b2b_header.php
+++ include/b2b/b2b_header.php
@@ -17,7 +17,7 @@
                 } ?>
                 ">Статьи</a></li>
                 <li><a href="./index.php?page=goods" style="
-                                    <?php if (isset($_GET['page'])) {
+                <?php if (isset($_GET['page'])) {
                     if ($_GET['page'] == "goods") {
                         echo "background-color: #FFFF00;";
                     }
@@ -66,42 +66,31 @@
                 </div>
 
                 <div class="burger__menu__content" id="burger-menu-content">
-                    <div class="burger__menu__languages">
-                        <a class="" href="#">
-                            <p>AZ</p>
-                        </a>
-                        <a class="burger__menu__languages__active" href="#">
-                            <p>RU</p>
-                        </a>
-                        <a class="" href="#">
-                            <p>ENG</p>
-                        </a>
-                    </div>
-                    <div class="burger__menu__login">
-                        <a href="./index.php?page=login">
-                            <p>Вход</p>
-                        </a>
-                    </div>
-                    <div class="burger__menu__registration">
-                        <a href="./index.php?page=registration">
-                            <p>Регистрация</p>
-                        </a>
-                    </div>
-                    <div class="burger__menu__links">
-                        <a href="./index.php?page=pages">
-                            <p>Статьи</p>
-                        </a>
-                        <a href="./index.php?page=goods">
-                            <p>Построй дом!</p>
-                        </a>
-                        <a href="./b2b.php">
-                            <p>B2B</p>
-                        </a>
-                        <a href="#">
-                            <p>О проекте</p>
-                        </a>
-                    </div>
+                    <?php
+                    if (isset($_GET['login'])) {
+                        if ($_GET['login'] == 'yes') {
+                            include './include/burger_mobile__loggedin.php';
+                        } else {
+                            include './include/burger_mobile.php';
+                        }
+                    } else {
+                        include './include/burger_mobile.php';
+                    }
+
+                    ?>
+
+
+
+
+
+
+
+
                 </div>
+
+
+
+
             </div>
         </div>
 
--- include/b2b/login.php
+++ include/b2b/login.php
@@ -1,6 +1,5 @@
 <style>
-    <?php include './style/main.css' ?>
-    <?php include './style/login.css' ?>
+    <?php include './style/main.css' ?><?php include './style/login.css' ?>
 </style>
 
 <div class="login__page__container">
@@ -8,23 +7,114 @@
         <p>ВХОД</p>
     </div>
     <div class="login__page__form">
-        <form action="#" method="post">
+        <form id="loginForm" action="#" method="post">
             <div class="login__input__container">
                 <input type="text" placeholder="Логин">
             </div>
             <div class="login__input__container">
                 <input type="text" placeholder="Пароль">
             </div>
+            <div class="login__input__password">
+                <button id="openPopup">Забыли пароль?</button>
+            </div>
+
+
+
             <div class="login__input__button">
                 <button>Вход</button>
             </div>
+
             <div class="login__input__button">
-                <a href="#">
+                <a href="./index.php?page=registration">
                     <p>Регистрация</p>
                 </a>
             </div>
         </form>
+        
+        <div id="popup" class="popup">
+            <div class="popup-content">
+                <span class="close" id="closePopup">&times;</span>
+                <div id="preloader" class="preloader">
+                    <div class="preloader__container">
+                        <div id="loader"></div>
+                    </div>
+                </div>
+                <form id="resetPassword" class="popup__container">
+                    <div id="popup__name" class="popup__name">
+                        <p>ВОССТАНОВЛЕНИЕ ПАРОЛЯ</p>
+                    </div>
+                    <div id="popup__input__container" class="popup__input__container">
+                        <input type="text" placeholder="Введите эмейл или номер телефона">
+                    </div>
+                    <div class="popup__reset">
+                        <button id="sendEmail">Восстановить</button>
+                    </div>
+
+                    <div id="congratulations" class="hidden">
+                        <div class="congratulations__container">
+                            <div class="congratulations__container__img">
+                                <img src="./imgs/ready.png" alt="">
+                            </div>
+                            <div class="congratulations__container__text">
+                                <p>Код восстановления отправлен на почту!</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
     </div>
-
-
 </div>
+
+<script>
+    document.getElementById('resetPassword').addEventListener("submit", (event) => {
+        event.preventDefault();
+    })
+    document.getElementById('loginForm').addEventListener("submit", (event) => {
+        event.preventDefault();
+    })
+    // Get the popup and button elements (same as before)
+    var popup = document.getElementById("popup");
+    var openPopupButton = document.getElementById("openPopup");
+    var closePopupButton = document.getElementById("closePopup");
+    var sendEmailButton = document.getElementById("sendEmail");
+    var preloader = document.getElementById("preloader");
+    var congratulations = document.getElementById("congratulations");
+
+    // Show the popup when the button is clicked (same as before)
+    openPopupButton.addEventListener("click", function() {
+        popup.style.display = "block";
+        document.addEventListener("click", closePopupOutside);
+    });
+
+    // Function to close the popup when clicking outside of it (same as before)
+    function closePopupOutside(event) {
+        if (event.target === popup) {
+            popup.style.display = "none";
+            document.removeEventListener("click", closePopupOutside);
+        }
+    }
+
+    // Close the popup when the close button is clicked (same as before)
+    closePopupButton.addEventListener("click", function() {
+        popup.style.display = "none";
+        document.removeEventListener("click", closePopupOutside);
+    });
+
+    // Send Email button click event
+    sendEmailButton.addEventListener("click", function() {
+        // Hide the current content and show the preloader
+        sendEmailButton.style.display = "none";
+        document.getElementById("popup__name").style.display = "none";
+        document.getElementById("popup__input__container").style.display = "none";
+        preloader.style.display = "block";
+
+        // Simulate sending email with a 2-second delay
+        setTimeout(function() {
+            preloader.style.display = "none";
+            congratulations.style.display = "block";
+        }, 2000);
+    });
+</script>
