00be315e1fdd37f732665f51291f7b25e19b05e5
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-10-19 01:45:00 +0400

    added

--- include/b2b/company.php
+++ include/b2b/company.php
@@ -188,15 +188,18 @@
                                     <!-- Swiper -->
                                     <div class="swiper mySwiper">
                                         <div class="swiper-wrapper">
-                                            <div class="swiper-slide">Slide 1</div>
-                                            <div class="swiper-slide">Slide 2</div>
-                                            <div class="swiper-slide">Slide 3</div>
-                                            <div class="swiper-slide">Slide 4</div>
-                                            <div class="swiper-slide">Slide 5</div>
-                                            <div class="swiper-slide">Slide 6</div>
-                                            <div class="swiper-slide">Slide 7</div>
-                                            <div class="swiper-slide">Slide 8</div>
-                                            <div class="swiper-slide">Slide 9</div>
+                                            <div class="swiper-slide">
+                                                <img src="./imgs/test_cert.jpg" alt="">
+                                                <p>Название сертификата</p>
+                                            </div>
+                                            <div class="swiper-slide">
+                                                <img src="./imgs/test_cert.jpg" alt="">
+                                                <p>Название сертификата</p>
+                                            </div>
+                                            <div class="swiper-slide">
+                                                <img src="./imgs/test_cert.jpg" alt="">
+                                                <p>Название сертификата</p>
+                                            </div>
                                         </div>
                                         <div class="swiper-button-next"></div>
                                         <div class="swiper-button-prev"></div>
@@ -535,6 +538,8 @@
   <!-- Initialize Swiper -->
   <script>
     var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+      freeMode: true,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
